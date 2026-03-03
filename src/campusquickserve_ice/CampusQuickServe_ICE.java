f4aba90a142ff048dff92c1058984d0f4532d69d
Luke Granelli <luke.granelli@gmail.com>
2026-03-03 19:46:24 +0200

    Variables added and basic invoice designed

--- src/campusquickserve_ice/CampusQuickServe_ICE.java
+++ src/campusquickserve_ice/CampusQuickServe_ICE.java
@@ -1,20 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package campusquickserve_ice;
 
-/**
- *
- * @author lukeg
- */
-public class CampusQuickServe_ICE {
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class CampusQuickServe_ICE {    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        String customerName = JOptionPane.showInputDialog("Enter customer name:");;
+        int studentNumber = 10412345;
+        String itemOrdered = "Burger";
+        int itemQuantity = 2;
+        double itemPrice = 45.50;
+        double subtotal = itemQuantity * itemPrice;
+        final double VAT = 0.15;
+        double vatValue = VAT * subtotal;
+        double total = vatValue + subtotal;
+        String headerMsg = "------CAMPUS QUICKSERVE------";
+        String footerMsg = "Thank you for your order!";
+        String invoiceEnd = "-------------------------------";
+        String lineBreak = "\n";
+        
+        
+        System.out.println(headerMsg);
+        System.out.println("Customer: " + customerName);
+        System.out.println("Student Number: " + studentNumber);
+        System.out.println(lineBreak);
+        
+        System.out.println("Item Ordered: " + itemOrdered);
+        System.out.println("Quantity: " + itemQuantity);
+        System.out.println("Price per Item: R" + itemPrice);
+        
+        System.out.println("Subtotal: R" + subtotal);
+        System.out.println("VAT (15%): R" + vatValue);
+        System.out.println("Total: R" + total);
+        System.out.println(lineBreak);
+        
+        System.out.println(footerMsg);
+        System.out.println(invoiceEnd);
+        
     }
     
 }
