68efe00362a5f31624e5ad8d586c3c5f8bff5a33
ST10482816 <ST10482816@vcconnect.edu.za>
2026-03-06 21:07:59 +0200

    Working project, requires format resolve on invoice

--- src/campusquickserve_ice/CampusQuickServe_ICE.java
+++ src/campusquickserve_ice/CampusQuickServe_ICE.java
@@ -9,13 +9,21 @@ public class CampusQuickServe_ICE {
     
     public class Order{
         
-        public static double calculateOrderPrice(final double VAT, String itemQuantity, String itemPrice){
-            double subtotal = Integer.parseInt(itemQuantity) * Integer.parseInt(itemPrice);
+        public static double[] calculateOrderPrice(final double VAT, int quantity, double price){
+            double subtotal = quantity * price;
             double vatValue = VAT * subtotal;
             double total = vatValue + subtotal;
-            return total;
+            return new double[] {subtotal, vatValue, total};
         }
-        public static void createInvoice(double total, double vatValue, double subtotal, String customerName, String studentNumber, String itemOrdered, String itemQuantity, String itemPrice){
+        public static String createInvoice(double total, 
+                double vatValue, 
+                double subtotal, 
+                String customerName, 
+                String studentNumber, 
+                String itemOrdered, 
+                String itemQuantity, 
+                String itemPrice){
+            
             String headerMsg = "------CAMPUS QUICKSERVE------";
             String footerMsg = "Thank you for your order!";
             String invoiceEnd = "-------------------------------";
@@ -35,7 +43,8 @@ public class CampusQuickServe_ICE {
                     
                     footerMsg + "\n" + 
                     invoiceEnd + "\n";
-                    
+         
+            return invoiceOutput;
         }
     }
     
@@ -46,34 +55,18 @@ public class CampusQuickServe_ICE {
         String itemQuantity = JOptionPane.showInputDialog("Enter quantity:");
         String itemPrice = JOptionPane.showInputDialog("Enter item price in rands:");
         
-//        double subtotal = Integer.parseInt(itemQuantity) * Integer.parseInt(itemPrice);
-//        double vatValue = VAT * subtotal;
-//        double total = vatValue + subtotal;
+        int quantity = Integer.parseInt(itemQuantity);
+        double price = Double.parseDouble(itemPrice);
         
         
+        double[] orderCalcResults = Order.calculateOrderPrice(VAT, quantity, price);
+        double subtotal = orderCalcResults[0];
+        double vatValue = orderCalcResults[1];
+        double total = orderCalcResults[2];
         
-//        String headerMsg = "------CAMPUS QUICKSERVE------";
-//        String footerMsg = "Thank you for your order!";
-//        String invoiceEnd = "-------------------------------";
-//        String lineBreak = "\n";
-//        
-//        
-//        System.out.println(headerMsg);
-//        System.out.println("Customer: " + customerName);
-//        System.out.println("Student Number: " + studentNumber);
-//        System.out.println(lineBreak);
-//        
-//        System.out.println("Item Ordered: " + itemOrdered);
-//        System.out.println("Quantity: " + itemQuantity);
-//        System.out.println("Price per Item: R" + itemPrice);
-//        
-//        System.out.println("Subtotal: R" + subtotal);
-//        System.out.println("VAT (15%): R" + vatValue);
-//        System.out.println("Total: R" + total);
-//        System.out.println(lineBreak);
-//        
-//        System.out.println(footerMsg);
-//        System.out.println(invoiceEnd);
+        String invoiceOutput = Order.createInvoice(total, vatValue, subtotal, customerName, studentNumber, itemOrdered, itemQuantity, itemPrice);
+        JOptionPane.showMessageDialog(null, invoiceOutput);
+//        System.out.println(invoiceOutput);
         
     }
     
